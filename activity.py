ad02bd9210a90ccb590a9285cd8ea819d1bfbf2f
SametDuhan <16sametduhan16@gmail.com>
2026-09-21 23:54:42 +0300

    Add Movie class and fix typos in activity.py
    Fix typos and add Movie class with methods.

--- activity.py
+++ activity.py
@@ -8,6 +8,21 @@ def wan(trr):
   if trr > 70:
     print("yıur number is bigger than 70")
 
+class Movie():
+    def __init__(self, title, director, duration):
+        self.title = title
+        self.director = director
+        self.duration = duration
+        print('movie objesi oluşturuldu.')
+
+    def __str__(self):
+        return f"{self.title} by {self.director}"
+
+    def __len__(self):
+        return self.duration
+
+    def __del__(self):
+        print('film objesi silindi')
 a = input("enter your number"):
 
 if a%2==0:
