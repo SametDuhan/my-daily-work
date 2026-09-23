25abbd31cb40578829d88dc56823eda219fb6937
SametDuhan <16sametduhan16@gmail.com>
2026-09-23 21:43:00 +0300

    Update adabd.py

--- adabd.py
+++ adabd.py
@@ -1,6 +1,6 @@
 from math import ceil , floor, sqrt
 from modulea  import add_a
-
+import random as rr
 
 while True:
     try:
@@ -10,7 +10,14 @@ while True:
     except Exception as e:
         print(f"you have a {e} problem check it")
     
+ent_number1=input("enter a number : ")
 
+if ent_number % 2 ==0:
+    return sqrt(ent_number)
 
+a = rr.random()
 
-
+if a <= ent_number:
+    if a * 100 == range(12,34):
+        print("your number is tge greatest number ever ")
+    
