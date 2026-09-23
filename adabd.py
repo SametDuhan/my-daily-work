500cd11fc0d68a9432a9a3aeba74bc684693d74c
SametDuhan <16sametduhan16@gmail.com>
2026-09-23 21:46:15 +0300

    Update adabd.py

--- adabd.py
+++ adabd.py
@@ -20,4 +20,10 @@ a = rr.random()
 if a <= ent_number:
     if a * 100 == range(12,34):
         print("your number is tge greatest number ever ")
-    
+    elif a*100 == range (34,67):
+        print("not the best number but still good")
+    elif a*100== range(67,100):
+        print("sorry you are not lucky today")
+    else:
+        pass
+
