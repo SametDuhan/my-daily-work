f48ad0c2a0cf6a45e278f62bd80efca0f5f7edc4
SametDuhan <16sametduhan16@gmail.com>
2026-09-23 21:37:59 +0300

    Add files via upload

---
+++ adabd.py
@@ -0,0 +1,16 @@
+from math import ceil , floor, sqrt
+from modulea  import add_a
+
+
+while True:
+    try:
+        enetered = int(input("Enter a number: "))
+        print(enetered.add_a())
+        break
+    except Exception as e:
+        print(f"you have a {e} problem check it")
+    
+
+
+
+
